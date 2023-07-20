b4f46fe18fdd8cad0684ee420633aeeba3f3627d
RohitSenta <rohitsenta@gmail.com>
2023-07-20 23:56:49 +0530

    Var, Let And Const Keywords

---
+++ JavaScript/Let-Const-And-Var-Keywords.js
@@ -0,0 +1,49 @@
+console.log("Let, Const And Var - Keywords\n\n");
+
+console.log("Let Keyword");
+console.log("Let Keyword is syntex of declaring variables\n\n");
+
+let age = 23;
+age = age + 1;
+console.log(`age is : ${age}`);
+
+let CGPA;
+CGPA = 8.13;
+console.log("CGPA is :", CGPA);
+
+let num1 = 30;
+let num2 = 15;
+let num3 = num1 + num2;
+console.log(`Sum of Num1 And Num2 is : ${num3}`);
+console.log("\n\n");
+
+
+console.log("Const Keyword");
+console.log("Const Keyword is value of constants can't be changed with re-assingment & they can't be re-declared \n\n");
+
+const year = 2023;
+console.log(`year is ${year}`);
+// year = year + 1;
+// console.log(`year is ${year}`);
+
+const pi = 3.14;
+console.log(`Value of pi is : ${pi}`);
+// pi = 4.5;
+// console.log(`Vlaue of pi is : ${pi}`);
+console.log("\n\n");
+
+
+console.log("Var Keyword");
+console.log("Var Keyword is a Old Syntex of Writing Variables \n\n");
+
+var name = "Rohit";
+console.log(`My name is ${name}`);
+
+var cpi = 7.6;
+console.log(`CPI is : ${cpi}`);
+
+var numA = 55;
+var numB = 33;
+var numC = numA - numB;
+console.log("Sum of NumA and NumB is :", numC);
+
