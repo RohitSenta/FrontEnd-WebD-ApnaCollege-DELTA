bee14ee47f8888b5763b9f2de6d88f132aef83b4
RohitSenta <rohitsenta@gmail.com>
2023-07-20 23:54:59 +0530

    JavaScript Basic Concepts

---
+++ JavaScript/app.js
@@ -0,0 +1,19 @@
+console.log("Hello World!");
+console.log("I am Rohit Senta");
+console.log("Apna College");
+
+let a = 10;
+let b = 5;
+console.log("Sum is : ", a + b);
+// this is a comment in JS
+
+let priceOne = 10;
+let priceTwo = 5;
+let pencilPrice = `Pencil price is : ${priceOne} Rupees.`; 
+console.log(pencilPrice);
+let erasorPrice = `Erasor Price is : ${priceTwo} Rupees.`;
+console.log(erasorPrice);
+
+let totalPrice = "Total Price is : " + (priceOne + priceTwo) + " Rupees.";
+console.log(totalPrice);
+console.log("\n");  
