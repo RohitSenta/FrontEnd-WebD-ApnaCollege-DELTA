6776273f9e890c714914a717338dc89e52038dfe
RohitSenta <rohitsenta@gmail.com>
2023-07-23 00:34:07 +0530

    Assignment Questions JS Part-6

--- JavaScript/Assignment-Questions-JS-Part-6.js
+++ JavaScript/Assignment-Questions-JS-Part-6.js
@@ -57,3 +57,40 @@ function longCon(contArr){
 }
 longCon(country);
 console.log(longest);
+console.log("\n\n");
+
+
+// Quetions 4
+console.log("Q-4 : Write a JavaScript function to count the number of vowels in String argument. \n\n");
+
+let strVow = "Apna College!!!";
+let count = 0;
+
+function vowels(str){
+    for(let i=0; i<str.length; i++){
+        if(str[i] == "a" || str[i] == "e" || str[i] == "i" || str[i] == "o" || str[i] == "u"){
+            count++;
+        }
+        else if(str[i] == "A" || str[i] == "E" || str[i] == "I" || str[i] == "O" || str[i] == "U"){
+            count++;
+        }
+    }
+    return count;
+}
+vowels(strVow);
+console.log("Number of Vowels in String :", count);
+console.log("\n\n");
+
+
+// Question 5
+console.log("Q-5 : Write a JavaScript function to generate a ramdom number within a range(start, end) \n\n");
+
+let start = 5;
+let end = 10;
+
+function generateRendon(start, end){
+    let random = Math.floor((Math.random() * (end-start)) + start);
+    console.log(`Random number between ${start} & ${end} is :`, random);
+}
+generateRendon(start, end);
+console.log("\n\n");
