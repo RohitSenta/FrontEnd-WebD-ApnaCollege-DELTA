3080f706749a3beb5e17e412d7b955a944bbb6d7
RohitSenta <rohitsenta@gmail.com>
2023-07-24 23:57:42 +0530

    Set-Timeout And Set-Interval Function

---
+++ JavaScript/Set-TimeOut-And-Set-Interval-Function.js
@@ -0,0 +1,28 @@
+// Set TimeOut Function
+
+console.log("Set TimeOut Function \n\n");
+
+console.log("Hi There !!!");
+
+setTimeout(() => {
+    console.log("Apna College !!!");
+}, 2000);
+
+console.log("Welcome to");
+console.log("\n\n");
+
+
+// Set Interval Function
+console.log("Set Interval Function \n\n");
+
+let id1 = setInterval(() => {
+    console.log("Hello World !!!");
+}, 3000);
+// clearInterval(id1)
+
+
+let id2 = setInterval(() => {
+    console.log("Hello JavaSccript !!!");
+}, 4000);
+// clearInterval(id2);
+
